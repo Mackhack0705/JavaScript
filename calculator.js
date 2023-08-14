0e3305f6c4c6f58b4a945964c042f79d6b5baaa1
Rajesh <malirajesh0705@gmail.com>
2023-08-14 15:02:34 +0530

    Calculator completed

--- calculator.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("input").onclick = function() {
-    document.getElementById("input").style.borderColor = "white";
-}
-
-document.querySelectorAll("btn").onclick = function() {
-    
-}
